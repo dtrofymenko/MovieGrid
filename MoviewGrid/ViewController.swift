45566cd20c051f32978fb13caf9a07be73bd6f9f
Dmytro Trofymenko <dmytro.trofymenko@cloudmade.com>
2019-06-09 12:11:55 +0300

    Initial Commit

---
+++ MoviewGrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviewGrid
+//
+//  Created by Dmytro Trofymenko on 6/9/19.
+//  Copyright © 2019 CloudMade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
